0a51aa4afc8274c560d7774686374a2c95e7a1c8
lxdnz254 <lxdnz34@gmail.com>
2014-12-21 11:57:43 +1300

    Update 4
    variables to lower case and removed "_", updated tidydata.txt file

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,12 @@
-# run_analysis.R
+## run_analysis.R
 
-## install packages, libraries, check working diretory and set
+## This script will download zip file, extract & tidy data producing
+## a data table with the average results from the mean and 
+## standard deviation results of the measurements observed from all
+## subjects doing all activities. The data table will be saved as a .txt
+## file in your working directory.
+
+# install packages, libraries, check working diretory and set
 
 install.packages(c("downloader", "dplyr", "tidyr", "stringr"))
 library(plyr)
@@ -10,14 +16,14 @@ library(stringr)
 WD <- getwd()
 if (!is.null(WD)) setwd(WD)
 
-## download, unzip, and extract raw data
+# download, unzip, and extract raw data
 
 url <- "http://d396qusza40orc.cloudfront.net/getdata/projectfiles/UCI HAR Dataset.zip"
 require(downloader)
 download(url, destfile = "./data.zip", mode = "wb")
 unzip("./data.zip", exdir = WD)
 
-## extract data from files
+# extract data from files
 
 features <- read.table("./UCI HAR Dataset/features.txt", quote="\"")
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"")
@@ -28,16 +34,17 @@ activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", quote="\"
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", quote="\"")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", quote="\"")
 
-## combine raw data to one data set, extracting mean & std columns
+# combine raw data to one data set, extracting mean & std columns
 
 data_X <- tbl_df(rbind(X_test, X_train))
 data_Y <- tbl_df(rbind(y_test, y_train))
 data_Z <- tbl_df(rbind(subject_test, subject_train))
 data_Y <- cbind(data_Z, data_Y)
-## name columns and extract means & stds
+
+# name columns and extract means & stds
 
 colnames(data_X) <- as.character(make.names(features$V2, unique = TRUE))
-colnames(data_Y) <- c("Subject", "Activity")
+colnames(data_Y) <- c("subject", "activity")
 data_XM <- select(data_X, contains("mean.."), contains("std.."), -contains("angle."))
 data_XY <- cbind(data_Y, data_XM)
 
@@ -45,20 +52,23 @@ data_XY <- cbind(data_Y, data_XM)
 
 agmean <- data.frame()
 for (i in 1:30){
-        agsplit <- subset(data_XY, Subject == i)
-        agtest <- aggregate(agsplit, by=list(agsplit$Activity), mean)
+        agsplit <- subset(data_XY, subject == i)
+        agtest <- aggregate(agsplit, by=list(agsplit$activity), mean)
         agtest <- agtest[,-1]
         agmean <- rbind(agmean, agtest)
 }
 agmean <- tbl_df(agmean)
-# Name activities
 
+# Name, make lower case and remove "_" from activities 
+activity_labels$V2 <- tolower(activity_labels$V2) %>%
+        str_replace_all("[[:punct:]]", "")
 activity <- activity_labels$V2
 oldvalues <- c("1", "2", "3", "4", "5", "6")
 newvalues <- factor(activity)  # Make this a factor
-agmean$Activity <- newvalues[ match(agmean$Activity, oldvalues) ]
+agmean$activity <- newvalues[ match(agmean$activity, oldvalues) ]
 
 # Name Mean Measurements
+
 columnlist <- colnames(agmean) %>%
         gsub(pattern = "BodyBody", replacement = "Body") %>% #fix double 'Body'
         gsub(pattern = "X", replacement ="on_the_X_axis") %>%
@@ -91,7 +101,7 @@ columnlist <- colnames(agmean) %>%
         gsub(pattern = "_AccM",
              replacement = "_Accelerated_M")
 
-## Bring "Mean" & "Standard Deviation" to start of Column
+# Bring "Mean" & "Standard Deviation" to start of Column
 
 quote <- c("Mean_Magnitude_of_", "Standard_Deviation_of_Magnitude_of_",
            "Mean_", "Standard_Deviation_of_")
@@ -104,11 +114,21 @@ for (k in 1:68) {
                                      replacement = "", columnlist[k])
                 columnlist[k] <- paste(quote[j],
                                        columnlist[k], sep ="")
-}}}       
- # put column list back into data.frame "agmean"
+}}}
+
+# Decapitalise all variables and remove "_" to make variables 'tidy'
+
+columnlist <- tolower(columnlist) %>%
+        str_replace_all("[[:punct:]]", "")
+        
+
+# put column list back into data.frame "agmean"
         
 colnames(agmean) <- columnlist
 
-#write data.frame(agmean) to .txt file
+# write data.frame(agmean) to .txt file
 
 write.table(agmean, "tidydata.txt", row.names = FALSE)
+
+## End of script: to view data table 
+## either view(agmean) or read.table("tidydata.txt")
